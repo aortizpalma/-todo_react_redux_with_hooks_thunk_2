43070c45e0f2c4f86fcc8505c09967e09a744ba2
Margit Tennosaar <margit.tennosaar@bc.fi>
2021-08-30 09:56:48 +0300

    first session code

---
+++ src/store/reducers/reducer.js
@@ -0,0 +1,23 @@
+import * as actionTypes from "../actions";
+
+const reducer = (state = [], action) => {
+  switch (action.type) {
+    case actionTypes.ADD_TODO:
+      return state.concat({
+        id: action.id + 1,
+        text: action.text,
+        completed: action.completed,
+      });
+    case actionTypes.TOGGLE_TODO:
+      const noteToChange = state.find((n) => n.id === action.id);
+      const changeNote = {
+        ...noteToChange,
+        completed: !noteToChange.completed,
+      };
+      return state.map((note) => (note.id !== action.id ? note : changeNote));
+    default:
+      return state;
+  }
+};
+
+export default reducer;
